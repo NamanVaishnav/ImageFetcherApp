8ec271f59c00cb6ecea6c350241acad432151cf4
Naman Vaishnav <namanmvaishnav@gmail.com>
2024-01-02 19:23:35 +0530

    Initial Commit

---
+++ ImageFetcherApp/ImageFetcherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFetcherAppApp.swift
+//  ImageFetcherApp
+//
+//  Created by Naman Vaishnav on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageFetcherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
